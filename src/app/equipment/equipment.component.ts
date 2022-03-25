2739a84ba8cad0f4fc59f447d175ef74c586ed3a
viji <viji4deepi@gmail.com>
2022-03-24 23:07:08 -0500

    bonus mission completed

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipments: string[] = ["Habitat dome","Drones","Food containers","Oxygen tanks" ];
+  equipmentBeingEdited: string;
+  /*
+<ul>
+    <li>Habitat dome</li>
+    <li>Drones</li>
+    <li>Food containers</li>
+    <li>Oxygen tanks</li>
+</ul>
+
+  */
+
   constructor() { }
 
   ngOnInit() {
   }
+
+  
+  add(equipmentName: string){
+
+    for(let toolName of this.equipments) {
+      if(toolName === equipmentName)
+      return;
+    }
+
+  this.equipments.push(equipmentName);
+}
+
+remove(equipment:string) {
+  let index = this.equipments.indexOf(equipment);
+  this.equipments.splice(index,1);
+}
+
+edit(equipment: string) {
+  this.equipmentBeingEdited = equipment;
+}
+
+save(name: string, equipment: string) {
+let index = this.equipments.indexOf(equipment);
+this.equipments[index] = name;
+}
+
+
 
 }
--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -6,10 +6,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./experiments.component.css']
 })
 export class ExperimentsComponent implements OnInit {
-
+  experiments: string[] = ["Mars soil sample","Drones","Plant growth in habitat","Human bone density" ];
+  experimentBeingEdited: string;
+  /*
+<ul>
+  <li>Mars soil sample</li>
+  <li>Plant growth in habitat</li>
+  <li>Human bone density</li>
+</ul>
+  */
   constructor() { }
 
   ngOnInit() {
   }
+
+  add(experimentName: string){
+
+    for(let toolName of this.experiments) {
+      if(toolName === experimentName)
+      return;
+    }
+
+  this.experiments.push(experimentName);
+}
+
+remove(experiment:string) {
+  let index = this.experiments.indexOf(experiment);
+  this.experiments.splice(index,1);
+}
+
+edit(experiment: string) {
+  this.experimentBeingEdited = experiment;
+}
+
+save(name: string, experiment: string) {
+let index = this.experiments.indexOf(experiment);
+this.experiments[index] = name;
+}
+
 
 }
